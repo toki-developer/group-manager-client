398197560604ee88246167caf59c9ec97185971a
Toki <55911995+tokio-k@users.noreply.github.com>
2021-03-27 23:24:14 +0900

    ESLint,Prettier導入

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,5 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 export default function Home() {
-  return (
-    <div >
-      Hello　World
-    </div>
-  )
+  return <div>Hello World</div>;
 }
