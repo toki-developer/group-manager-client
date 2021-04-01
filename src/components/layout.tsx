93753135fe5b1a3e1658d54e1897239ce3a709de
Toki <55911995+tokio-k@users.noreply.github.com>
2021-04-01 22:05:14 +0900

    update/Layout修正

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -6,15 +6,15 @@ export const Layout = (props: { children: ReactNode }) => {
   return (
     <div className={"min-h-screen bg-black text-white"}>
       <div className={"max-w-3xl mx-auto lg:grid lg:max-w-5xl lg:grid-cols-12"}>
+        <div className={"hidden lg:block col-span-3 "}>
+          <SideMenu />
+        </div>
         <div
           className={
-            "lg:col-span-9 lg:order-2 min-h-screen border-l border-r border-gray-600"
+            "lg:col-span-9 min-h-screen border-l border-r border-gray-600"
           }
         >
           {props.children}
-        </div>
-        <div className={"hidden lg:block col-span-3 order-1 "}>
-          <SideMenu />
         </div>
         <div className={"lg:hidden fixed bottom-0 w-full max-w-3xl bg-red-500"}>
           <FixedMenu />
