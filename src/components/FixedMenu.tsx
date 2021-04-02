7d5393dacae9d1b5249a6b44469e41eeb5117f9f
Toki <55911995+tokio-k@users.noreply.github.com>
2021-04-02 11:55:52 +0900

    ADD/固定メニュー作成

--- src/components/FixedMenu.tsx
+++ src/components/FixedMenu.tsx
@@ -1,7 +1,41 @@
+import cc from "classcat";
+import { useRouter } from "next/dist/client/router";
+import Link from "next/link";
+import { MENU } from "src/utils/constants/menu";
+
 export const FixedMenu = () => {
+  const router = useRouter();
   return (
-    <div>
-      <div>fixedmenu</div>
-    </div>
+    <nav className={" flex justify-around"}>
+      {MENU.map((item) => {
+        const isActive = item.href == router.pathname;
+        return (
+          <Link key={item.label} href={item.href}>
+            <a>
+              <svg
+                className={cc([
+                  "flex-shrink-0 my-3 h-7 w-7 md:my-4 md:h-8 md:w-8",
+                  {
+                    ["text-green-500"]: isActive,
+                  },
+                ])}
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d={item.icon}
+                />
+              </svg>
+            </a>
+          </Link>
+        );
+      })}
+    </nav>
   );
 };
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,7 +5,7 @@ import { SideMenu } from "src/components/SideMenu";
 export const Layout = (props: { children: ReactNode }) => {
   return (
     <div className={"min-h-screen bg-black text-white"}>
-      <div className={"max-w-3xl mx-auto lg:grid lg:max-w-5xl lg:grid-cols-12"}>
+      <div className={"max-w-3xl mx-auto lg:grid lg:max-w-6xl lg:grid-cols-12"}>
         <div className={"hidden lg:block col-span-3 "}>
           <SideMenu />
         </div>
@@ -16,7 +16,11 @@ export const Layout = (props: { children: ReactNode }) => {
         >
           {props.children}
         </div>
-        <div className={"lg:hidden fixed bottom-0 w-full max-w-3xl bg-red-500"}>
+        <div
+          className={
+            "lg:hidden fixed bottom-0 w-full max-w-3xl bg-black border-l border-r border-t border-gray-600"
+          }
+        >
           <FixedMenu />
         </div>
       </div>
