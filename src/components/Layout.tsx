ad2c4db95d075d58a23837b418bd293dfaf9264b
Toki <55911995+tokio-k@users.noreply.github.com>
2021-05-26 16:24:09 +0900

    update/リファクタリング

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -18,16 +18,12 @@ export const Layout = (props: { children: ReactNode }) => {
     router.push("/mypage");
   }
   return (
-    <div className={"min-h-screen bg-black"}>
-      <div className={"max-w-3xl mx-auto lg:grid lg:max-w-6xl lg:grid-cols-12"}>
-        <div className={"hidden lg:block col-span-3 text-gray-300"}>
+    <div className="min-h-screen bg-black">
+      <div className="max-w-3xl mx-auto lg:grid lg:max-w-6xl lg:grid-cols-12">
+        <div className="hidden lg:block col-span-3 text-gray-300">
           <SideMenu />
         </div>
-        <div
-          className={
-            "lg:col-span-9 min-h-screen border-l border-r border-gray-600 text-white"
-          }
-        >
+        <div className="lg:col-span-9 min-h-screen border-l border-r border-gray-600 text-white">
           {loading && (
             <div className="fixed w-full h-screen flex items-center justify-center">
               <div className="animate-spin rounded-full border-t-4 border-green-500 h-8 w-8" />
@@ -36,11 +32,7 @@ export const Layout = (props: { children: ReactNode }) => {
           {user.id == "" && !loading && <Login />}
           {!loading && props.children}
         </div>
-        <div
-          className={
-            "lg:hidden fixed bottom-0 w-full max-w-3xl bg-black border-l border-r border-t border-gray-600 text-gray-300 h-14 md:h-16"
-          }
-        >
+        <div className="lg:hidden fixed bottom-0 w-full max-w-3xl bg-black border-l border-r border-t border-gray-600 text-gray-300 h-14 md:h-16">
           <FixedMenu />
         </div>
         <div className="lg:hidden h-14 md:h-16"></div>
--- src/components/Top.tsx
+++ src/components/Top.tsx
@@ -9,20 +9,12 @@ type Props = {
 export const Top = (props: Props) => {
   const group = useContext(GroupContext);
   return (
-    <div
-      className={
-        " sticky top-0 bg-black border-gray-600 border-b border-l border-r"
-      }
-    >
-      <div className={"flex items-center h-16"}>
+    <div className=" sticky top-0 bg-black border-gray-600 border-b border-l border-r">
+      <div className="flex items-center h-16">
         {props.title !== "マイページ" && (
           <Icon iconUrl={group.group?.iconUrl} />
         )}
-        <div
-          className={
-            "inline-block align-middle ml-3 text-2xl font-font-bold text-gray-300"
-          }
-        >
+        <div className="inline-block align-middle ml-3 text-2xl font-font-bold text-gray-300">
           {props.title}
         </div>
       </div>
